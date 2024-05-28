a5c56cd908c3521d0c05091635c578f4accc311a
Nemsh <Artnemshilov@gmail.com>
2024-05-28 18:44:05 +0200

    DB model created

--- tree/models.py
+++ tree/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    date_of_employment = models.DateField()
+    email = models.EmailField(max_length=255, unique=True)
+    chief = models.ForeignKey(
+        'self', on_delete=models.SET_NULL(), null=True, blank=True,
+        help_text="The chief of the employee. Can be null if not assigned."
+    )
+
+    class Meta:
+        ordering = ['full_name']
+        # added indexes for search optimization
+        indexes = [
+            models.Index(fields=['position']),
+            models.Index(fields=['email']),
+        ]
+
+    def __str__(self):
+        return self.full_name, self.position, self.email
